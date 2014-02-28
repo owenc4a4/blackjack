1815547f5c8c4998d520df0b93ea4632629f7fae
donghao-cloud <owenc4a4@gmail.com>
2014-02-28 15:41:03 +0900

    add ruby test

---
+++ rails-test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTest::Application.config.secret_key_base = 'ddfc06cc93eafbe4aca1d0e9d3f22215f67484b6c4cdfbacfc6bdbac4bf28c6fb6c366b6a6fca950fd4192ea2694a673429b8801e1d64089343c0f21ddcea760'
